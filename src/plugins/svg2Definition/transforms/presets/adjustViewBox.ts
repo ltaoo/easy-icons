94ff3523eba0f4cb96decc339502b201cf09d543
litao <litao@weipaitang.com>
2021-02-20 20:10:15 +0800

    fix: 图标比直接使用 svg 更大的问题

--- src/plugins/svg2Definition/transforms/presets/adjustViewBox.ts
+++ src/plugins/svg2Definition/transforms/presets/adjustViewBox.ts
@@ -1,36 +1,6 @@
-import { assignAttrsAtTag } from '..';
-import { TransformFactory } from '../..';
-import { includes } from 'ramda';
+import { assignAttrsAtTag } from "..";
+import { TransformFactory } from "../..";
 
-// version < antd@3.9
-const OLD_ICON_NAMES = [
-  'step-backward',
-  'step-forward',
-  'fast-backward',
-  'fast-forward',
-  'forward',
-  'backward',
-  'caret-up',
-  'caret-down',
-  'caret-left',
-  'caret-right',
-  'retweet',
-  'swap-left',
-  'swap-right',
-  'loading',
-  'loading-3-quarters',
-  'coffee',
-  'bars',
-  'file-jpg',
-  'inbox',
-  'shopping-cart',
-  'safety',
-  'medium-workmark'
-];
-
-export const adjustViewBox: TransformFactory = assignAttrsAtTag(
-  'svg',
-  ({ name }) => ({
-    viewBox: includes(name, OLD_ICON_NAMES) ? '0 0 1024 1024' : '64 64 896 896'
-  })
-);
+export const adjustViewBox: TransformFactory = assignAttrsAtTag("svg", () => ({
+  viewBox: "0 0 1024 1024",
+}));
